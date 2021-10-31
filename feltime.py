62e6afc54dd053d53920bb0abf375db154595de3
MarkUsenson <timurkatime@gmail.com>
2021-10-31 22:20:44 +0100

    Update feltime.py

--- feltime.py
+++ feltime.py
@@ -2,25 +2,57 @@ import sqlite3
 
 subjects = []
 
+class DATABASE:
+
+    def __init__(self, file_db):
+        self.connect = sqlite3.connect(file_db)
+        self.cursor = self.connect.cursor()
+
+    def close(self):
+        self.connect.close()
+
+class Client(DATABASE):
+    def __init__(self,user,file_db):
+        super().__init__(file_db)
+        self.user = user 
+
+    def user_check(self):
+        result = self.cursor.execute("SELECT 'id' FROM 'users' WHERE 'user' = ?",(self.user,))
+        return bool(len(result.fetchall()))
+
+    def get_user_id(self):
+        user_id = self.cursor.execute("SELECT 'id' FROM 'users' WHERE 'user' = ?",(self.user,))
+        return user_id.fetchone()[0]
+
+    def add_user(self):
+        self.cursor.execute("INSERT INTO 'users' ('user') VALUES (?)",(self.user,))
+        return self.connect.commit()
+
 class Subject:
     def __init__(self,name, code, kred, stat):
         self.name = name
         self.code = code
         self.kred = kred #pocet kreditu
         self.stat = stat #statistika uspechu
+
     def kredits(self):
         return self.kred
+
     def statistics(self):
         return self.stat
+
     def jadro_v1_kred(self):
         kredit_part = self.kredits()/allkredits*100
         return kredit_part 
+
     def jadro_v1_stat(self): 
         statistics_part = self.statistics()/allstats*100
         return statistics_part
+
     def jadro_v1(self):
         part = ((self.jadro_v1_kred() + self.jadro_v1_stat())/2)
         return part
+
     def jadro_v2(self):
         a = tests()
         b = subjective()
@@ -33,39 +65,51 @@ class Subject:
         part = (func_results[subjects.index(self)] + subj_results[subjects.index(self)])/2    
         return part 
 
-
-class DATABASE:
-
+class Data(DATABASE):
     def __init__(self, file_db):
-        self.connect = sqlite3.connect(file_db)
-        self.cursor = self.connect.cursor()
+        super().__init__(file_db)
 
-    def user_check(self, user):
-        result = self.cursor.execute("SELECT 'id' FROM 'users' WHERE 'user' = ?",(user,))
-        return bool(len(result.fetchall()))
+    def subjective(): 
+        scores = []
+        for subject in subjects:
+            subject.score = int(input('Jaký máte pocit ze předmětu {}? Ohodnoťte svůj pocit od 1 do 10 (1 - vůbec mi nejde; 10 - zvládám uplně všechno):'.format(subject.name)))
+            scores.append(subject.score)
+        for subject in subjects:
+            subject.procent = subject.score/sum(scores)*100 
+        goal_procent = 100/len(scores)
+        parts_subjectives = {}
+        for subject in subjects:
+            subject.delta_multiply = 1 + (goal_procent - subject.procent)/100
+            part_subjective = subject.jadro_v1() * subject.delta_multiply
+            parts_subjectives[subject] = part_subjective
+        return parts_subjectives
 
-    def get_user_id(self,user):
-        user_id = self.cursor.execute("SELECT 'id' FROM 'users' WHERE 'user' = ?",(user,))
-        return user_id.fetchone()[0]
-
-    def add_user(self, user):
-        self.cursor.execute("INSERT INTO 'users' ('user') VALUES (?)",(user,))
-        return self.connect.commit()
+    def tests(): 
+        bods = []
+        for subject in subjects:
+            subject.bod = int(input('Jaký máte poslední výsledek ze předmětu "{}"? Uveďte výsledek v procentech (P.S. Jestli nemáte žádný výsledek, uveďte v procentech svůj subjektivní pocit ze předmětu):'.format(subject.name)))
+            bods.append(subject.bod)
+        for subject in subjects:
+            subject.procent = subject.bod/sum(bods)*100 
+        goal_procent = 100/len(bods)
+        parts_subjectives = {}
+        for subject in subjects:
+            subject.delta_multiply = 1 + (goal_procent - subject.procent)/100
+            part_subjective = subject.jadro_v1() * subject.delta_multiply
+            parts_subjectives[subject] = part_subjective
+        return parts_subjectives
 
     def add_subject(self,subject):
         pass
         """
         EDIT HERE!!!
+        FOR ADMINS 
         """
+
     
 
-    def close(self):
-        self.connect.close()
-
-
-
-
 ################################################################################
+
 '''Subjects: *kod predmetu* = Subject(*pocet kreditu*, *procent NEuspechu*):'''
 laga = Subject('Lineární algebra','laga', 7, 40); subjects.append(laga)
 uela = Subject('Úvod do elektrotechniky','uela', 4, 5); subjects.append(uela)
@@ -73,39 +117,5 @@ ma1a = Subject('Matematická analýza','ma1a', 6, 20); subjects.append(ma1a)
 
 allkredits = 7 + 4 + 6
 allstats = 40 + 5 + 20
+
 #################################################################################
-
-def subjective(): 
-    scores = []
-    for subject in subjects:
-        subject.score = int(input('Jaký máte pocit ze předmětu {}? Ohodnoťte svůj pocit od 1 do 10 (1 - vůbec mi nejde; 10 - zvládám uplně všechno):'.format(subject.name)))
-        scores.append(subject.score)
-    for subject in subjects:
-        subject.procent = subject.score/sum(scores)*100 
-    goal_procent = 100/len(scores)
-    parts_subjectives = {}
-    for subject in subjects:
-        subject.delta_multiply = 1 + (goal_procent - subject.procent)/100
-        part_subjective = subject.jadro_v1() * subject.delta_multiply
-        parts_subjectives[subject] = part_subjective
-    return parts_subjectives
-
-def tests(): 
-    bods = []
-    for subject in subjects:
-        subject.bod = int(input('Jaký máte poslední výsledek ze předmětu "{}"? Uveďte výsledek v procentech (P.S. Jestli nemáte žádný výsledek, uveďte v procentech svůj subjektivní pocit ze předmětu):'.format(subject.name)))
-        bods.append(subject.bod)
-    for subject in subjects:
-        subject.procent = subject.bod/sum(bods)*100 
-    goal_procent = 100/len(bods)
-    parts_subjectives = {}
-    for subject in subjects:
-        subject.delta_multiply = 1 + (goal_procent - subject.procent)/100
-        part_subjective = subject.jadro_v1() * subject.delta_multiply
-        parts_subjectives[subject] = part_subjective
-    return parts_subjectives
-""" 
-database = DATABASE('database.db')
-database.add_user('GOAL')
-"""
-
