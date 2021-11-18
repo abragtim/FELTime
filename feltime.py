835a672b63d836ce835386472fbeb59a13d36cdb
MarkUsenson <timurkatime@gmail.com>
2021-11-18 11:04:04 +0100

    upd data

--- feltime.py
+++ feltime.py
@@ -200,11 +200,11 @@ class Data(DATABASE):
 ################################################################################
 
 '''Subjects: *kod predmetu* = Subject(*pocet kreditu*, *procent NEuspechu*):'''
-BAB31AF1 = Subject('Základy anatomie a fyziologie I','BAB31AF1', 4, 10); 
-B0B01LAGA = Subject('Lineární algebra','B0B01LAGA', 7, 30); 
-B2B15UELA = Subject('Úvod do elektrotechniky','B2B15UELA', 4, 10); 
-B0B01MA1A = Subject('Matematická analýza','B0B01MA1A', 6, 17); 
-BAB37ZPR = Subject('Základy programování','BAB37ZPR', 6, 10); 
+BAB31AF1 = Subject('Základy anatomie a fyziologie I','BAB31AF1', 4, 14); 
+B0B01LAGA = Subject('Lineární algebra','B0B01LAGA', 7, 47.08); 
+B2B15UELA = Subject('Úvod do elektrotechniky','B2B15UELA', 4, 16.67); 
+B0B01MA1A = Subject('Matematická analýza','B0B01MA1A', 6, 44.64); 
+BAB37ZPR = Subject('Základy programování','BAB37ZPR', 6, 22.78); 
 subjects_list = [BAB31AF1, B0B01LAGA, B0B01MA1A, B2B15UELA,BAB37ZPR]
 
 #################################################################################
